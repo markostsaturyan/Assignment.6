50df327a2932cb46d1c99f0ce4809d424244bc09
markostsaturyan <markostsaturyan@gmail.com>
2018-03-16 23:51:16 +0400

    Add files via upload

---
+++ DictNode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries
+{
+    public class DictNode<TKey, TValue>
+    {
+        public ColorEnum Color;
+
+        public DictNode<TKey, TValue> Parent;
+        public DictNode<TKey, TValue> Left;
+        public DictNode<TKey, TValue> Right;
+
+        public KeyValuePair<TKey, TValue> Info;
+
+        public DictNode(KeyValuePair<TKey, TValue> info, DictNode<TKey, TValue> parent, DictNode<TKey, TValue> left = null, DictNode<TKey, TValue> right = null, ColorEnum color = ColorEnum.Red)
+        {
+            this.Info = info;
+            this.Parent = parent;
+            this.Left = left;
+            this.Right = right;
+            this.Color = color;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Dictionaries;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedBlackDictionary<int, int> redBlack = new RedBlackDictionary<int, int>();
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+            Stopwatch time = new Stopwatch();
+            Random random = new Random();
+
+            int a = 320;
+            time.Start();
+            while (a != 0)
+            {
+                redBlack.Add(a, random.Next(1,150));
+                a--;
+            }
+            time.Stop();
+            Console.WriteLine("320 elements added in red-black dictionary: " + time.Elapsed.TotalMilliseconds + "miliseconds");
+            time.Reset();
+
+            a = 320;
+            time.Start();
+            while (a != 0)
+            {
+                redBlack.Add(a, random.Next(1, 150));
+                a--;
+            }
+            time.Stop();
+            Console.WriteLine("320 elements added in C# dictionary: " + time.Elapsed.TotalMilliseconds + "miliseconds");
+            time.Reset();
+
+            redBlack.Clear();
+            dictionary.Clear();
+
+            a = 640;
+            time.Start();
+            while (a != 0)
+            {
+                redBlack.Add(a, random.Next(1, 150));
+                a--;
+            }
+            time.Stop();
+            Console.WriteLine("640 elements added in red-black dictionary: " + time.Elapsed.TotalMilliseconds + "miliseconds");
+            time.Reset();
+
+            a = 640;
+            time.Start();
+            while (a != 0)
+            {
+                redBlack.Add(a, random.Next(1, 150));
+                a--;
+            }
+            time.Stop();
+            Console.WriteLine("640 elements added in C# dictionary: " + time.Elapsed.TotalMilliseconds + "miliseconds");
+            time.Reset();
+
+            redBlack.Clear();
+            dictionary.Clear();
+
+            a = 1280;
+            time.Start();
+            while (a != 0)
+            {
+                redBlack.Add(a, random.Next(1, 150));
+                a--;
+            }
+            time.Stop();
+            Console.WriteLine("1280 elements added in red-black dictionary: " + time.Elapsed.TotalMilliseconds + "miliseconds");
+            time.Reset();
+
+            a = 1280;
+            time.Start();
+            while (a != 0)
+            {
+                redBlack.Add(a, random.Next(1, 150));
+                a--;
+            }
+            time.Stop();
+            Console.WriteLine("1280 elements added in C# dictionary: " + time.Elapsed.TotalMilliseconds + "miliseconds");
+            time.Reset();
+
+            Console.Read();
+        }
+    }
+}
